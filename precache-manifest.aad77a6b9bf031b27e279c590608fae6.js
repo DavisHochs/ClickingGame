f4a01268502ea92b70be814a4f8c900d19ec1600
Davis Hochstatter <davishochs@gmail.com>
2020-01-12 15:32:59 -0600

    Updates

---
+++ precache-manifest.aad77a6b9bf031b27e279c590608fae6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a69aa709795a15ac6cf31dd8cf0035ab",
+    "url": "/ClickingGame/index.html"
+  },
+  {
+    "revision": "b8ae05fd356d8df785f9",
+    "url": "/ClickingGame/static/css/main.5a33fb49.chunk.css"
+  },
+  {
+    "revision": "722b8d453d2f967b6293",
+    "url": "/ClickingGame/static/js/2.538d43e3.chunk.js"
+  },
+  {
+    "revision": "21f38193c3caf91fe32157540d33cdde",
+    "url": "/ClickingGame/static/js/2.538d43e3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b8ae05fd356d8df785f9",
+    "url": "/ClickingGame/static/js/main.48091b1c.chunk.js"
+  },
+  {
+    "revision": "342e49731648dde2a4dd",
+    "url": "/ClickingGame/static/js/runtime-main.1d6f7e70.js"
+  }
+]);
